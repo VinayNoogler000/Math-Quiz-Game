76b8c640212034ccca8d80e5b2c454eb9e26c1a9
VinayNoogler000 <vinaytambey000@gmail.com>
2024-10-17 23:26:50 +0530

    Added a 'scoreUpd' element, which displays an animation on score updation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Selecting all the elements to work with:
     const scoreValEl = document.querySelector("#scoreVal");
+    const scoreUpdEl = document.querySelector("#scoreUpd"); 
     const questionEl = document.querySelector("#question");
     const formEl = document.querySelector("form");
     const userInpEl = formEl.querySelector("#answerInp");
@@ -70,12 +72,56 @@ document.addEventListener("DOMContentLoaded", () => {
         return [operand1, operand2, operation];
     }
 
-    // Function to get a new question, a correct answer, empty the input field, & to reset the 'wrongAnsCount'.
-    const updateGameBox = () => {
+    // Function to get a new question, calculate the correct answer, empty the input field, & to reset the 'wrongAnsCount'.
+    const updateGameBoxEls = () => {
         questionArr = generateQuestion();
         correctAns = calculateAns(questionArr[0], questionArr[1], questionArr[2]); 
         userInpEl.value = "";
         wrongAnsCount = 0; //resets to '0' zero, when the user gives a correct answer.
+    }
+
+    const modifyScoreUpdEl = (isAnsCorrect) => {
+        /* Animation Removal & Reflow technique is used to avoid any unexpected behaviour, 
+        when the animations are added & removed, rapidly, on user interactions. */
+        scoreUpdEl.classList.remove("animate-[scoreUpdation_1s_ease-out_0s_1_normal]"); //Animation Removal
+        void scoreUpdEl.offsetWidth; // Force reflow[a process in which browser recalculates the layout of the webpage(parts of it), normally happens when we make changes to the DOM(structure of the webpage) or styling of it's elements(sizing & position)]
+
+        if(isAnsCorrect) { //then, modify the text content, make the text green, and apply animation to the element:
+            scoreUpdEl.innerText = "+3";
+            scoreUpdEl.classList.add("text-green-400", "animate-[scoreUpdation_1s_ease-out_0s_1_normal]");
+        }
+        else { //then, modify the text content, make the text red, and apply animation to the element.
+            scoreUpdEl.innerText = "-1";
+            scoreUpdEl.classList.add("text-red-600", "animate-[scoreUpdation_1s_ease-out_0s_1_normal]");
+        }
+
+        // Remove the text-colors and the animation, when, once the animation ends:
+        scoreUpdEl.addEventListener('animationend', () => {
+            scoreUpdEl.classList.remove("text-green-400", "text-red-600", "animate-[scoreUpdation_1s_ease-out_0s_1_normal]"); 
+        });
+    }
+
+    const updateScore = (isAnsCorrect) => {
+        if(isAnsCorrect) { //Increment the Score by 3
+            score += 3;
+            scoreValEl.innerText = score;
+            modifyScoreUpdEl(true);
+            updateGameBoxEls();
+        }
+        else { // Decrement the Score by 1, and Update the Wrong Answers Count:
+            scoreValEl.innerText = --score;
+            modifyScoreUpdEl(false);
+            wrongAnsCount++;
+
+            // When "wrongAnsCount" has crossed the limit of 3, then Generate a New Question, by calling "updateGameBox()":
+            if(wrongAnsCount < 3) {
+                alert("You've entered wrong answer. Please, retry again!");
+            }
+            else {
+                alert(`YOU'VE ENTERED THE WRONG ANSWER, THRICE.\nThe Correct Answer is ${correctAns}.\nLet's Move On to the Next Question...!\n\n*Note: If the question asks you to perform division operation, then the answer should be rounded to two decimal places (only, if the answer contains decimal values).`);
+                updateGameBoxEls(); 
+            }
+        }
     }
 
     // Initialization of Game-Box [Score & Question]:
@@ -84,32 +130,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let correctAns = calculateAns(questionArr[0], questionArr[1], questionArr[2]);
     let wrongAnsCount = 0; 
 
-
-    // Handling Form Submit Event and Update the Score:
+    // Handling Form (Answer) Submit Event, by Updating the Game-Box:
     formEl.addEventListener("submit", (event) => {
         event.preventDefault();
 
         if(userInpEl.value === String(correctAns)) { 
-            
-            //Increment the Score by 3 & Update the Game-Box:
-            score += 3;
-            scoreValEl.innerText = score; 
-            updateGameBox();
+            updateScore(true);
         }
         else {
-            
-            //Update the Wrong Answers Count and Decrement the Score by 1:
-            wrongAnsCount++;
-            scoreValEl.innerText = --score; 
-
-            // When "wrongAnsCount" has crossed the limit of 3, then Generate a New Question, by calling "updateGameBox()":
-            if(wrongAnsCount < 3) {
-                alert("You've entered wrong answer. Please, retry again!");
-            }
-            else {
-                alert(`YOU'VE ENTERED THE WRONG ANSWER, THRICE.\nThe Correct Answer is ${correctAns}.\nLet's Move On to the Next Question...!\n\n*Note: If the question asks you to perform division operation, then the answer should be rounded to two decimal places (only, if the answer contains decimal values).`);
-                updateGameBox(); 
-            }
+            updateScore(false);
         }
     });
 });
