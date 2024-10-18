c83cfb85dbcd062c7b30e487da80d0d640dc7f1b
VinayNoogler000 <vinaytambey000@gmail.com>
2024-10-18 16:31:36 +0530

    Added a Feature of Locally Storing & Deleting the User's Score from the Broswer

--- src/app.js
+++ src/app.js
@@ -5,48 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const questionEl = document.querySelector("#question");
     const formEl = document.querySelector("form");
     const userInpEl = formEl.querySelector("#answerInp");
-
-    const generateRandomNum = (start, end) => {
-        // Ensure the start is less than the end:
-        if (start > end) {
-            throw new Error("Start value must be less than end value, in the range.");
-        }
-
-        // Calculate the range:
-        const range = (end - start) + 1; //to include both the start and end values, it's incremented by 1.
-
-        // Generate a random number within the range and add the start value:
-        const randomNumber = Math.floor(Math.random() * range) + start;
-
-        return randomNumber;
-    }
-
-    const generateRandomOperation = () => {
-        const operandsArr = ["add", "subtract", "multiply", "divide"];
-        let operation = operandsArr[ generateRandomNum(0, 3) ]; //to randomly select an opeartion from the opreandArr[].
-        return operation;
-    }
-
-    const calculateAns = (operand1, operand2, operation) => {
-        let correctAns;
-
-        // Calculate the Answer, based on the Operation:
-        if(operation.toLowerCase() === "add") {
-            correctAns = operand1 + operand2;
-        }
-        else if(operation.toLowerCase() === "subtract") {
-            correctAns = operand1 - operand2;
-        }
-        else if(operation.toLowerCase() === "multiply") {
-            correctAns = operand1 * operand2;
-        }
-        else if(operation.toLowerCase() === "divide") {
-            //round to number upto 2 decimal places 👇
-            correctAns = Math.round((operand1 / operand2) * 100) / 100; 
-        }
-        
-        return correctAns;
-    }
+    const scoreStoragePromptMsg = "Do you want to Save your Score?\n-Enter 'Yes' to Store the Score.\n-Enter 'No' or Click 'Cancel' button to Deny Storing the Score, or If the Score is Already Stored.\n-Enter 'Delete' to Remove the Stored Score.\n\nUsage:\nThis Feature will Allow you to Continue your Game from the Same Score, where you Quit at the Last Game Session, even if you Close the Website/Browser.";
 
     const generateQuestion = () => {
         // Generate Two Random Operands and One Random Operation:
@@ -72,12 +31,71 @@ document.addEventListener("DOMContentLoaded", () => {
         return [operand1, operand2, operation];
     }
 
-    // Function to get a new question, calculate the correct answer, empty the input field, & to reset the 'wrongAnsCount'.
-    const updateGameBoxEls = () => {
-        questionArr = generateQuestion();
-        correctAns = calculateAns(questionArr[0], questionArr[1], questionArr[2]); 
-        userInpEl.value = "";
-        wrongAnsCount = 0; //resets to '0' zero, when the user gives a correct answer.
+    const calculateAns = (operand1, operand2, operation) => {
+        let correctAns;
+
+        // Calculate the Answer, based on the Operation:
+        if(operation.toLowerCase() === "add") {
+            correctAns = operand1 + operand2;
+        }
+        else if(operation.toLowerCase() === "subtract") {
+            correctAns = operand1 - operand2;
+        }
+        else if(operation.toLowerCase() === "multiply") {
+            correctAns = operand1 * operand2;
+        }
+        else if(operation.toLowerCase() === "divide") {
+            //round to number upto 2 decimal places 👇
+            correctAns = Math.round((operand1 / operand2) * 100) / 100; 
+        }
+        
+        return correctAns;
+    }
+
+    const updateScore = (isAnsCorrect) => {
+        if(isAnsCorrect) { //Increment the Score by 3
+            score += 3;
+            localStorage.setItem("score", score); //update the locally stored 'score'
+            scoreValEl.innerText = score;
+            modifyScoreUpdEl(true);
+            updateGameBoxEls();
+        }
+        else { // Decrement the Score by 1, and Update the Wrong Answers Count:
+            localStorage.setItem("score", --score); //update the locally stored 'score'
+            scoreValEl.innerText = score;
+            modifyScoreUpdEl(false);
+            wrongAnsCount++;
+
+            // When "wrongAnsCount" has crossed the limit of 3, then Generate a New Question, by calling "updateGameBox()":
+            if(wrongAnsCount < 3) {
+                alert("You've entered wrong answer. Please, retry again!");
+            }
+            else {
+                alert(`YOU'VE ENTERED THE WRONG ANSWER, THRICE.\nThe Correct Answer is ${correctAns}.\nLet's Move On to the Next Question...!\n\n*Note: If the question asks you to perform division operation, then the answer should be rounded to two decimal places (only, if the answer contains decimal values).`);
+                updateGameBoxEls(); 
+            }
+        }
+    }
+
+    const generateRandomNum = (start, end) => {
+        // Ensure the start is less than the end:
+        if (start > end) {
+            throw new Error("Start value must be less than end value, in the range.");
+        }
+
+        // Calculate the range:
+        const range = (end - start) + 1; //to include both the start and end values, it's incremented by 1.
+
+        // Generate a random number within the range and add the start value:
+        const randomNumber = Math.floor(Math.random() * range) + start;
+
+        return randomNumber;
+    }
+
+    const generateRandomOperation = () => {
+        const operandsArr = ["add", "subtract", "multiply", "divide"];
+        let operation = operandsArr[ generateRandomNum(0, 3) ]; //to randomly select an opeartion from the opreandArr[].
+        return operation;
     }
 
     const modifyScoreUpdEl = (isAnsCorrect) => {
@@ -101,31 +119,44 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    const updateScore = (isAnsCorrect) => {
-        if(isAnsCorrect) { //Increment the Score by 3
-            score += 3;
-            scoreValEl.innerText = score;
-            modifyScoreUpdEl(true);
-            updateGameBoxEls();
-        }
-        else { // Decrement the Score by 1, and Update the Wrong Answers Count:
-            scoreValEl.innerText = --score;
-            modifyScoreUpdEl(false);
-            wrongAnsCount++;
+    // Function to get a new question, calculate the correct answer, empty the input field, & to reset the 'wrongAnsCount'.
+    const updateGameBoxEls = () => {
+        questionArr = generateQuestion();
+        correctAns = calculateAns(questionArr[0], questionArr[1], questionArr[2]); 
+        userInpEl.value = "";
+        wrongAnsCount = 0; //resets to '0' zero, when the user gives a correct answer.
+    }
 
-            // When "wrongAnsCount" has crossed the limit of 3, then Generate a New Question, by calling "updateGameBox()":
-            if(wrongAnsCount < 3) {
-                alert("You've entered wrong answer. Please, retry again!");
+    // Function to display a prompt, which asks the user, if they want to store their score locally or not:
+    const showScoreStoragePrompt = (message) => {
+        userReplyToPrompt = prompt(message);
+        
+        if(userReplyToPrompt === null || userReplyToPrompt.toLowerCase() === "no") {
+            console.log("User Don't Want to Store the Score.");
+        }
+        else if(userReplyToPrompt.toLowerCase() === "yes") { //when, user wants to store the score
+            if(localStorage.getItem("score") === null) {
+                localStorage.setItem("score", score);
             }
-            else {
-                alert(`YOU'VE ENTERED THE WRONG ANSWER, THRICE.\nThe Correct Answer is ${correctAns}.\nLet's Move On to the Next Question...!\n\n*Note: If the question asks you to perform division operation, then the answer should be rounded to two decimal places (only, if the answer contains decimal values).`);
-                updateGameBoxEls(); 
+            else { //when, user wants to store the score, but the score is already stored
+                alert("Invalid Reply! Your Score is Already Stored.");
+                showScoreStoragePrompt(scoreStoragePromptMsg);
             }
+        }
+        else if(userReplyToPrompt.toLowerCase() === "delete") { //when, user wants to delete the stored score
+            localStorage.removeItem("score");
+            scoreValEl.innerText = score = 0;
+        }
+        else { //when, user entered an invalid value, other than "YES", "NO", or "DELETE"
+            console.log("Invalid Reply for Score Storage Prompt!");
+            alert("Invalid Reply! Please, Enter YES or NO, only.");
+            showScoreStoragePrompt(scoreStoragePromptMsg);
         }
     }
 
     // Initialization of Game-Box [Score & Question]:
-    let score = 0;
+    let score = localStorage.getItem("score") === null ? 0 : parseInt(localStorage.getItem("score"));
+    scoreValEl.innerText = score; //to display the locally stored "score" [if exists].
     let questionArr = generateQuestion();
     let correctAns = calculateAns(questionArr[0], questionArr[1], questionArr[2]);
     let wrongAnsCount = 0; 
@@ -141,4 +172,8 @@ document.addEventListener("DOMContentLoaded", () => {
             updateScore(false);
         }
     });
+
+    setTimeout( () => {
+        showScoreStoragePrompt(scoreStoragePromptMsg);
+    }, 500); //after 0.5s "score-storage" prompt will be displayed.
 });
